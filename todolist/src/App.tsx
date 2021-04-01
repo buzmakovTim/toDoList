d8b59ca592c778cba3d1cbca833b872d32b5a19d
buzmakovTim <74148963+buzmakovTim@users.noreply.github.com>
2021-03-31 19:36:06 -0700

    Update from Windows PC

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed Updated From Home
+          Changed Updated From Windows Computer
         </p>
         <a
           className="App-link"
